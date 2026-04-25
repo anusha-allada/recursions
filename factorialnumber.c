28c7524118c94a201a27eab2c2d0f660cd55aacd
anusha-allada <alladaanusha29@gmail.com>
2026-04-25 12:30:02 +0530

    Add files via upload

---
+++ factorialnumber.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int output(int i,int fact){
+    if(i<1)
+    {
+        return fact;
+    }
+    fact=fact*i;
+    output(i-1,fact);
+}
+int main()
+{
+    int n;
+    printf("enter the number \n");
+    scanf("%d",&n);
+    int res=output(n,1);
+    printf("the factorial of %d is %d",n,res);
+}
